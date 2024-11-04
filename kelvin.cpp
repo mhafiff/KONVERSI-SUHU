4c25f46dedaa8f48ee70b9fc9517d73e59ea3fc5
hnafive <mhdhafifnaibaho@gmail.com>
2024-11-04 18:24:50 +0700

    Update kelvin.cpp

--- kelvin.cpp
+++ kelvin.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -24,4 +24,4 @@ int main() {
     cout << "\nDibuat oleh Muhammad Hafif Naibaho\n";
 
     return 0;
-}*/
+}
